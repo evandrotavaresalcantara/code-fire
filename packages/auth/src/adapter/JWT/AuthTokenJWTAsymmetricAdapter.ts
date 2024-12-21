55c09d2ea07c98c18f6b2ea6f5e52694aee9b624
Braulio Henrique Marques Souto <braulio@disroot.org>
2024-12-20 21:44:02 -0300

    Criado adaptador AuthTokenJWTAsymmetricAdapter

---
+++ packages/auth/src/adapter/JWT/AuthTokenJWTAsymmetricAdapter.ts
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+import { readFileSync } from "node:fs";
+import path from "node:path";
+import { AuthToken } from "../../provider";
+
+export class AuthTokenJWTAsymmetricAdapter implements AuthToken {
+  private currentDir = path.dirname(__filename);
+  private readonly publicKey = readFileSync(
+    path.join(this.currentDir, "public.pem"),
+    "utf-8"
+  );
+  private readonly privateKey = readFileSync(
+    path.join(this.currentDir, "private.pem"),
+    "utf-8"
+  );
+
+  decode(token: string): string | object | null {
+    return jwt.decode(token, { json: true });
+  }
+
+  verify(token: string): boolean {
+    try {
+      jwt.verify(token, this.publicKey, {
+        algorithms: ["RS256"],
+      });
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  create(payload: string | Buffer | object, expires: string | number): string {
+    return jwt.sign(payload, this.privateKey, {
+      algorithm: "RS256",
+      expiresIn: expires,
+    });
+  }
+}
